52db1ef2fc63d7b26041fbec113259c8ab68cf0c
Sergey Seryiza Zaborovsky <seryiza@yandex.ru>
2018-07-14 15:16:46 +1100

    Добавил структуры для достижений

--- api/achievements.go
+++ api/achievements.go
@@ -2,23 +2,36 @@ package api
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/url"
 	"strconv"
+	"time"
 )
 
 const (
 	getAchievementsPath = "/api/achievements"
 )
 
+// Achievements of some user
+type Achievements []Achievement
+
+// Achievement of Shikimori
+type Achievement struct {
+	ID     int32  `json:"id"`
+	NekoID string `json:"neko_id"`
+	Level  int32  `json:"level"`
+
+	// Progress has int8 because it changes 0..100 (?)
+	Progress int8  `json:"progress"`
+	UserID   int32 `json:"user_id"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 // GetAchievements implemets GET /api/achievements
 // https://shikimori.org/api/doc/1.0/achievements/index
-func (shiki *Shikimori) GetAchievements(userID int) (json.RawMessage, error) {
-	// todo: как появится больше информации об этом, сделать более удобное
-	// 		   получение информации (без json)
-
+func (shiki *Shikimori) GetAchievements(userID int) (Achievements, error) {
 	strUserID := strconv.Itoa(userID)
-
 	query := url.Values{
 		"user_id": {strUserID},
 	}
@@ -29,10 +42,11 @@ func (shiki *Shikimori) GetAchievements(userID int) (json.RawMessage, error) {
 		return nil, err
 	}
 
-	json, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
+	jr := json.NewDecoder(resp.Body)
+	achs := make(Achievements, 0)
+	if err = jr.Decode(&achs); err != nil {
 		return nil, err
 	}
 
-	return json, nil
+	return achs, nil
 }
