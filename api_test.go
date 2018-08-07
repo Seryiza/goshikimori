da64178e5e71f3ab98efa114996bbd0187e5619c
Seryiza <seryiza@yandex.ru>
2018-08-07 22:00:32 +1100

    Добавлен тест непосредственно Shikimori API

---
+++ api_test.go
@@ -0,0 +1,30 @@
+package goshikimori_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/seryiza/goshikimori/helpers"
+	"github.com/seryiza/goshikimori/structs"
+)
+
+func TestGetUser(t *testing.T) {
+	userID := 206253
+	wantNickname := "Seryiza"
+	method := fmt.Sprintf("users/%d", userID)
+
+	shiki, err := helpers.GetShikimori("1.0")
+	if err != nil {
+		t.Error(err)
+	}
+	defer helpers.SaveToken(shiki)
+
+	user := &structs.User{}
+	if _, err = shiki.JSONGet(method, user); err != nil {
+		t.Error(err)
+	}
+
+	if user.Nickname != wantNickname {
+		t.Errorf("Given nickname '%v' is not '%v'", user.Nickname, wantNickname)
+	}
+}
