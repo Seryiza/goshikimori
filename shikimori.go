9a52d6137f91c2231086ed8769a4e38393bf56d9
Seryiza <seryiza@yandex.ru>
2018-08-07 21:52:58 +1100

    Добавлена проверка на 404 в Do; Добавлен метод JSONPost; Добавлены комментарии

--- shikimori.go
+++ shikimori.go
@@ -1,7 +1,9 @@
 package goshikimori
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -49,11 +51,23 @@ func (shiki *Shikimori) NewRequest(httpMethod, shikiMethod string, body io.Reade
 	return http.NewRequest(httpMethod, fullURL, body)
 }
 
+// Do http request.
+// If returns 404, return as error (for comfy use)
 func (shiki *Shikimori) Do(req *http.Request) (*http.Response, error) {
-	// todo: добавить обработку не-API ошибок (напр., HTTP 404)
-	return shiki.Client.Do(req)
+	resp, err := shiki.Client.Do(req)
+	if err != nil {
+		return resp, err
+	}
+
+	// check HTTP 404
+	if resp.StatusCode == http.StatusNotFound {
+		return resp, errors.New("Shikimori API method not found")
+	}
+
+	return resp, err
 }
 
+// Get Shikimori method (HTTP GET request)
 func (shiki *Shikimori) Get(method string) (*http.Response, error) {
 	req, err := shiki.NewRequest("GET", method, nil)
 	if err != nil {
@@ -63,6 +77,7 @@ func (shiki *Shikimori) Get(method string) (*http.Response, error) {
 	return shiki.Do(req)
 }
 
+// Post Shikimori method (HTTP POST request)
 func (shiki *Shikimori) Post(method string, body io.Reader) (*http.Response, error) {
 	req, err := shiki.NewRequest("POST", method, body)
 	if err != nil {
@@ -72,6 +87,7 @@ func (shiki *Shikimori) Post(method string, body io.Reader) (*http.Response, err
 	return shiki.Do(req)
 }
 
+// Delete Shikimori method (HTTP DELETE request)
 func (shiki *Shikimori) Delete(method string) (*http.Response, error) {
 	req, err := shiki.NewRequest("DELETE", method, nil)
 	if err != nil {
@@ -81,6 +97,7 @@ func (shiki *Shikimori) Delete(method string) (*http.Response, error) {
 	return shiki.Do(req)
 }
 
+// Put Shikimori method (HTTP PUT request)
 func (shiki *Shikimori) Put(method string, body io.Reader) (*http.Response, error) {
 	req, err := shiki.NewRequest("PUT", method, body)
 	if err != nil {
@@ -90,6 +107,7 @@ func (shiki *Shikimori) Put(method string, body io.Reader) (*http.Response, erro
 	return shiki.Do(req)
 }
 
+// Patch Shikimori method (HTTP PATCH request)
 func (shiki *Shikimori) Patch(method string, body io.Reader) (*http.Response, error) {
 	req, err := shiki.NewRequest("PATCH", method, body)
 	if err != nil {
@@ -99,6 +117,7 @@ func (shiki *Shikimori) Patch(method string, body io.Reader) (*http.Response, er
 	return shiki.Do(req)
 }
 
+// JSONGet Shikimori method (HTTP GET request + decode json into outStruct)
 func (shiki *Shikimori) JSONGet(method string, outStruct interface{}) (*http.Response, error) {
 	resp, err := shiki.Get(method)
 	if err != nil {
@@ -108,3 +127,23 @@ func (shiki *Shikimori) JSONGet(method string, outStruct interface{}) (*http.Res
 	err = json.NewDecoder(resp.Body).Decode(outStruct)
 	return resp, err
 }
+
+// JSONPost Shikimori method (HTTP POST request +
+// encode json from inStruct as body + decode json into outStruct)
+func (shiki *Shikimori) JSONPost(method string, inStruct, outStruct interface{}) (*http.Response, error) {
+	jsonBytes, err := json.Marshal(inStruct)
+	if err != nil {
+		return nil, err
+	}
+	jsonReader := bytes.NewReader(jsonBytes)
+
+	resp, err := shiki.Post(method, jsonReader)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(outStruct)
+	return resp, err
+}
+
+// todo: JSONDelete/Put/Patch
