ed408d8562e25d3e44e42c2fac0f777861edd248
Seryiza <seryiza@yandex.ru>
2018-08-15 15:18:25 +1100

    Добавлен подпакет для методов и форматов

---
+++ methods/users.go
@@ -0,0 +1,11 @@
+package methods
+
+const (
+	// Users - List users
+	// GET /api/users
+	Users = "users"
+
+	// UsersInfo - Show an user
+	// GET /api/users/:id/info
+	UsersInfo = "users/%d/info"
+)
